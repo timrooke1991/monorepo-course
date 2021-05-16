1d5cf5e4cbf90b51b59c7c7ccb8938cdd8b2dcb9
Tim Rooke <tim@axis.ws>
2021-05-16 15:37:16 +0100

    install

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -22,6 +22,5 @@
  * @packageDocumentation
  */
 
-// Testing
 export { isChannel, isMessage, isTeam, isTypedArray } from "./type-guards";
 export { IChannel, IMessage, ITeam, IUser } from "./types";
