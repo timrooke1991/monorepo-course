1ffea6b82bb002836caa15d96ff162df8889e273
Tim Rooke <tim@axis.ws>
2021-05-16 15:35:39 +0100

    add Notes to types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -21,5 +21,7 @@
  *
  * @packageDocumentation
  */
+
+// Testing
 export { isChannel, isMessage, isTeam, isTypedArray } from "./type-guards";
 export { IChannel, IMessage, ITeam, IUser } from "./types";
